4b784e5c153fa50a4b51909e3b23f5cffcd11f72
MantasMikal <mantas.codes@gmail.com>
2021-07-06 13:37:23 +0300

    init

---
+++ pages/_app.js
@@ -0,0 +1,7 @@
+import '@/assets/scss/base.global.scss'
+
+function App({ Component, pageProps }) {
+  return <Component {...pageProps} />
+}
+
+export default App
---
+++ pages/index.js
@@ -0,0 +1,5 @@
+const Home = () => {
+  return <div>Thanks for your help.</div>
+}
+
+export default Home
